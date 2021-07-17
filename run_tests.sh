1e19e6ac56a65e8c3ea910aef428f9ad11530816
filipp.chachin <filipp.chachin@x5.ru>
2021-07-17 15:43:12 +0300

    поменял вид метода fill_input в отчете allure

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,3 @@
 rm -r allure_results
-pytest -s -v -m mobile --mode="mobile" --reruns 5 --alluredir=allure_results
+pytest -s -v -m desktop --mode="desktop" --reruns 5 --alluredir=allure_results
 allure serve allure_results
